cf2d7b45993dd3bcb675c61b8c54ebccd4779af0
shun1249844726 <1249844726@qq.com>
2017-01-17 13:22:19 +0800

    增减权限设置

---
+++ app/src/main/java/com/qiantao/rxble/MainActivity.java
@@ -0,0 +1,134 @@
+package com.qiantao.rxble;
+
+import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.qiantao.rxble.ble.RxBle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
+import rx.functions.Action1;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ACCESS_COARSE_LOCATION = 1;
+
+    @BindView(R.id.tv_receive)
+    TextView sendTv;
+
+    @BindView(R.id.et_send)
+    EditText sendEt;
+
+    private String mMsgSend;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RxBle mRxBle;
+
+    private StringBuffer mStringBuffer;
+
+    BluetoothDevice mBleDevice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        verifyIfRequestPermission();
+        mStringBuffer = new StringBuffer();
+        mRxBle = RxBle.getInstance().setTargetDevice("test01");
+        mRxBle.openBle(this);
+        mRxBle.scanBleDevices(true);
+        mRxBle.receiveData().subscribe(new Action1<String>() {
+            @Override
+            public void call(String receiveData) {
+
+                System.out.println("receivedata:"+receiveData);
+                mRxBle.sendData(receiveData, 0);
+
+                sendTv.setText(mStringBuffer.append(receiveData));
+            }
+        });
+        mRxBle.setScanListener(new RxBle.BleScanListener() {
+            @Override
+            public void onBleScan(BluetoothDevice bleDevice, int rssi, byte[] scanRecord) {
+                // Get list of devices and other information
+                System.out.println("name:"+bleDevice.getName()+";");
+                if (bleDevice.getName().contains("test")){
+                    System.out.println("conn");
+                    mRxBle.connectDevice(bleDevice);
+                }
+            }
+        });
+
+        Button connectble = (Button) findViewById(R.id.connectbleBtn);
+        connectble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                mRxBle.connectDevice(mBleDevice);
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_send)
+    public void sendMessage(View view) {
+        if (!TextUtils.isEmpty(mMsgSend)) {
+            Log.d(TAG, "sendMessage: " + mMsgSend);
+            mRxBle.sendData(mMsgSend, 0);
+        }
+    }
+
+    @OnTextChanged(R.id.et_send)
+    public void onTextChanged(CharSequence text) {
+        mMsgSend = text.toString();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRxBle.closeBle();
+    }
+    private void verifyIfRequestPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            Log.i(TAG, "onCreate: checkSelfPermission");
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "onCreate: Android 6.0 动态申请权限");
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    Log.i(TAG, "*********onCreate: shouldShowRequestPermissionRationale**********");
+                    Toast.makeText(this, "只有允许访问位置才能搜索到蓝牙设备", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_ACCESS_COARSE_LOCATION);
+                }
+            } else {
+//                showDialog(getResources().getString(R.string.scanning));
+//                mBleService.scanLeDevice(true);
+
+            }
+        } else {
+//            showDialog(getResources().getString(R.string.scanning));
+//            mBleService.scanLeDevice(true);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/qiantao/rxble/ble/RxBle.java
@@ -0,0 +1,261 @@
+package com.qiantao.rxble.ble;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * Created by qiantao on 2016/11/24.
+ * Use RxJava help Bluetooth Low Energy device to communicate with your Android phone
+ */
+
+public class RxBle {
+
+    private static final long SCAN_PERIOD = 30000;
+    private static final String TAG = RxBle.class.getSimpleName();
+    private static final String UUID = "0000fff6-0000-1000-8000-00805f9b34fb";
+    private Context mContext;
+
+    private BluetoothAdapter mBleAdapter;
+    private boolean mIsScanning;
+    private static String sTargetDeviceName;
+    private BluetoothGatt mBleGatt;
+    private BluetoothGattCharacteristic mBleGattChar;
+
+    private Subject<String, String> mBus;
+
+    public interface BleScanListener {
+        /**
+         * Callback in BLE scanning,then you should use the method {@link #connectDevice} to connect target device
+         * @param bleDevice Identifies the remote device
+         * @param rssi The RSSI value for the remote device as reported by the Bluetooth hardware. 0 if no RSSI value is available
+         * @param scanRecord The content of the advertisement record offered by the remote device.
+         */
+        void onBleScan(BluetoothDevice bleDevice, int rssi, byte[] scanRecord);
+    }
+
+    private BleScanListener mScanListener;
+
+    /**
+     * Set listener on device scanning
+     * @param scanListener Listener of scaning
+     */
+    public void setScanListener(BleScanListener scanListener) {
+        mScanListener = scanListener;
+    }
+
+    private RxBle() {
+        mBus = new SerializedSubject<>(PublishSubject.<String>create());
+    }
+
+    public static RxBle getInstance() {
+        return Singleton.INSTANCE;
+    }
+
+    public RxBle setTargetDevice(String deviceName) {
+        sTargetDeviceName = deviceName;
+        return Singleton.INSTANCE;
+    }
+
+    private static class Singleton {
+        private static final RxBle INSTANCE = new RxBle();
+    }
+
+    public void openBle(Context context) {
+        mContext = context.getApplicationContext();
+        BluetoothManager bluetoothManager =
+                (BluetoothManager) this.mContext.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBleAdapter = bluetoothManager.getAdapter();
+        if (mBleAdapter != null) {
+            mBleAdapter.enable();
+        }
+    }
+
+    public void scanBleDevices(boolean enable) {
+        if (enable) {
+            Log.d(TAG, "scanBleDevices");
+            mIsScanning = true;
+            mBleAdapter.startLeScan(mBleScanCallback);
+            Observable.timer(SCAN_PERIOD, TimeUnit.MILLISECONDS).subscribe(new Action1<Long>() {
+                @Override
+                public void call(Long aLong) {
+                    mIsScanning = false;
+                    mBleAdapter.stopLeScan(mBleScanCallback);
+                }
+            });
+        } else {
+            mIsScanning = false;
+            mBleAdapter.stopLeScan(mBleScanCallback);
+        }
+    }
+
+    /**
+     * Callback when scanning BLE devices
+     */
+    private BluetoothAdapter.LeScanCallback mBleScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice bleDevice, int rssi, byte[] scanRecord) {
+            if (mIsScanning) {
+                if (bleDevice.getName() != null) {
+                    Log.d(TAG, "onLeScan: find device: " + bleDevice.getName());
+                    if (sTargetDeviceName != null && sTargetDeviceName.equals(bleDevice.getName())) {
+                        connectDevice(bleDevice);
+                    } else if (mScanListener != null) {
+                        mScanListener.onBleScan(bleDevice, rssi, scanRecord);
+                    }
+                }
+            } else {
+                Log.d(TAG, "onLeScan: stop scan");
+            }
+        }
+    };
+
+
+    /**
+     * Connect BLE devices
+     *
+     * @param bleDevice Target device you want to connect
+     */
+    public void connectDevice(BluetoothDevice bleDevice) {
+        scanBleDevices(false);
+        mBleGatt = bleDevice.connectGatt(mContext,
+                true,//true mean that can auto reconnect after disconnect
+                new BleGattCallback());
+        mBleGatt.connect();
+        Log.d(TAG, "connectDevice: start to connect " + mBleGatt.getDevice().getName());
+    }
+
+    /**
+     * Callback after device has been connected
+     */
+    private class BleGattCallback extends BluetoothGattCallback {
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt bleGatt, int status, int newState) {
+            super.onConnectionStateChange(bleGatt, status, newState);
+            Log.d(TAG, "onConnectionStateChange: " + newState);
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Log.d(TAG, "onConnectionStateChange: device connected");
+                //Discover services will call the next override method: onServicesDiscovered
+                bleGatt.discoverServices();
+            } else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+                Log.d(TAG, "onConnectionStateChange: device disconnected");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(final BluetoothGatt bleGatt, int status) {
+            Log.d(TAG, "onServicesDiscovered: services size: " + bleGatt.getServices().size());
+            List<BluetoothGattService> serviceList = bleGatt.getServices();
+            Observable.from(serviceList)
+                    .flatMap(new Func1<BluetoothGattService, Observable<BluetoothGattCharacteristic>>() {
+                        @Override
+                        public Observable<BluetoothGattCharacteristic> call(BluetoothGattService bleGattService) {
+                            return Observable.from(bleGattService.getCharacteristics());
+                        }
+                    })
+                    .filter(new Func1<BluetoothGattCharacteristic, Boolean>() {
+                        @Override
+                        public Boolean call(BluetoothGattCharacteristic bleGattChar) {
+                            return bleGattChar.getUuid().toString().equals(UUID);
+                        }
+                    })
+                    .subscribe(new Action1<BluetoothGattCharacteristic>() {
+                        @Override
+                        public void call(BluetoothGattCharacteristic bleGattChar) {
+                            //
+                            bleGatt.setCharacteristicNotification(bleGattChar, true);
+                            mBleGattChar = bleGattChar;
+                        }
+                    });
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, "onCharacteristicChanged");
+            String receiveData = new String(characteristic.getValue());
+            Log.d(TAG, "receive BLE 's data :" + receiveData);
+            Observable.just(receiveData)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Action1<String>() {
+                        @Override
+                        public void call(String receiveData) {
+                            mBus.onNext(receiveData);
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Send data to BLE device
+     *
+     * @param data the data will send to BLE device
+     */
+    public void sendData(String data) {
+        sendData(data, 0);
+    }
+
+    /**
+     * Send data to BLE device with delay
+     *
+     * @param data String will be send to BLE device
+     * @param time Delay time , milliseconds
+     */
+    public void sendData(final String data, long time) {
+        if (!mBleAdapter.isEnabled() || mBleAdapter == null || !mBleGatt.connect()) {
+            Log.d(TAG, "sendData: BLE is disconnected");
+            return;
+        }
+        Observable.timer(time, TimeUnit.MILLISECONDS)
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long l) {
+                        if (mBleGatt != null && mBleGattChar != null) {
+                            mBleGattChar.setValue(data);
+                            boolean isSend = mBleGatt.writeCharacteristic(mBleGattChar);
+                            Log.d(TAG, "send " + (isSend ? "success" : "fail"));
+
+
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Receive the data from BLE device
+     *
+     * @return Subject you should subscribed
+     */
+    public Observable<String> receiveData() {
+        return mBus;
+    }
+
+    public void closeBle() {
+        Log.d(TAG, "close BLE");
+        if (mBleGatt != null) {
+            mBleGatt.close();
+            mBleGatt.disconnect();
+        }
+        if (mBleAdapter != null) {
+            mBleAdapter.cancelDiscovery();
+        }
+        mBus.onCompleted();
+    }
+}
