778d388f266b54c619af9bb41e33dacb9b36c08c
shun1249844726 <1249844726@qq.com>
2017-01-17 14:56:56 +0800

    woye buzhid

--- app/src/main/java/com/qiantao/rxble/MainActivity.java
+++ app/src/main/java/com/qiantao/rxble/MainActivity.java
@@ -1,13 +1,20 @@
 package com.qiantao.rxble;
 
+import android.Manifest;
 import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.qiantao.rxble.ble.RxBle;
 
@@ -18,6 +25,7 @@ import butterknife.OnTextChanged;
 import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ACCESS_COARSE_LOCATION = 1;
 
     @BindView(R.id.tv_receive)
     TextView sendTv;
@@ -33,27 +41,39 @@ public class MainActivity extends AppCompatActivity {
 
     private StringBuffer mStringBuffer;
 
+    BluetoothDevice mBleDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        verifyIfRequestPermission();
         mStringBuffer = new StringBuffer();
-        mRxBle = RxBle.getInstance().setTargetDevice("Test");//为名字
+        mRxBle = RxBle.getInstance().setTargetDevice("test01");
         mRxBle.openBle(this);
         mRxBle.scanBleDevices(true);
         mRxBle.receiveData().subscribe(new Action1<String>() {
             @Override
             public void call(String receiveData) {
-                sendTv.setText(mStringBuffer.append(receiveData).append("\n"));
+
+                System.out.println("receivedata:"+receiveData);
+                mRxBle.sendData(receiveData, 0);
+
+                sendTv.setText(mStringBuffer.append(receiveData));
             }
         });
         mRxBle.setScanListener(new RxBle.BleScanListener() {
             @Override
             public void onBleScan(BluetoothDevice bleDevice, int rssi, byte[] scanRecord) {
                 // Get list of devices and other information
+                System.out.println("name:"+bleDevice.getName()+";");
+                if (bleDevice.getName().contains("test")){
+                    System.out.println("conn");
+                    mRxBle.connectDevice(bleDevice);
+                }
             }
         });
+        
     }
 
     @OnClick(R.id.btn_send)
@@ -74,4 +94,34 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         mRxBle.closeBle();
     }
+    private void verifyIfRequestPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            Log.i(TAG, "onCreate: checkSelfPermission");
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "onCreate: Android 6.0 动态申请权限");
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    Log.i(TAG, "*********onCreate: shouldShowRequestPermissionRationale**********");
+                    Toast.makeText(this, "只有允许访问位置才能搜索到蓝牙设备", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_ACCESS_COARSE_LOCATION);
+                }
+            } else {
+//                showDialog(getResources().getString(R.string.scanning));
+//                mBleService.scanLeDevice(true);
+
+            }
+        } else {
+//            showDialog(getResources().getString(R.string.scanning));
+//            mBleService.scanLeDevice(true);
+
+        }
+    }
+
 }
